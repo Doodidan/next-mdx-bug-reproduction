e4aa90e3831b305f8769040c329d6249c98c35fc
George Romanov <gosha.egorian@gmail.com>
2023-03-25 21:09:40 +0800

    Add minimal reproduction changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const MDX = require('@next/mdx');
+
+const withMDX = MDX({
+  extension: /\.mdx?$/
+});
+
 /** @type {import("next").NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 }
+
+module.exports = withMDX(nextConfig);
